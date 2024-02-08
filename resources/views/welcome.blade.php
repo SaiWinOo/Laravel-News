a9f6a10bb518b7d1cdee9c51a9323de7d84c2b36
SaiWinOo <saiwinoo52@gmail.com>
2024-02-08 14:21:48 +0630

    update home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,40 @@
                 @endforeach
             </div>
 
+            <section class="py-12 ">
+                <div class="mx-auto  w-full max-w-2xl px-6 lg:max-w-7xl">
+                    <div class="relative shadow-lg">
+                        <img class="absolute  -top-10 right-16 z-10 lg:-top-6 lg:left-1/3 lg:right-auto duration-1000 animate-bounce" src="https://picperf.io/https://laravel-news.com/images/cube.svg" alt="Cube">
+                        <div class="relative flex flex-wrap items-center justify-between gap-8 overflow-hidden rounded-lg border border-gray-100 bg-white p-8 shadow-card lg:flex-nowrap">
+                            <span class="absolute inset-y-0 left-0 w-1 bg-red-600"></span>
+                            <div>
+                                <h2 class="text-2xl font-bold sm:text-3xl">Laravel Newsletter</h2>
+                                <p class="mt-1 text-gray-600">
+                                    Join 40k+ other developers and never miss out on new tips, tutorials, and more.
+                                </p>
+                            </div>
+                            <div class="lg:shrink-0">
+                                <div>
+                                     <form>
+                                        <div class="flex w-full flex-wrap items-stretch gap-4">
+                                            <label class="relative flex min-w-[240px] flex-1 items-center bg-white">
+                                                <span class="sr-only">Email</span>
+                                                <img src="https://picperf.io/https://laravel-news.com/images/icons/newsletter.svg" alt="Newsletter icon" class="pointer-events-none absolute  left-3 top-3">
+                                                <input type="text" class="w-full rounded-lg border-gray-100 bg-transparent px-12 py-3 text-gray-600 placeholder-gray-600/50 transition focus:border-gray-100 focus:bg-gray-100/40 focus:outline-none focus:ring-2 focus:ring-red-600/80 focus:ring-offset-2" placeholder="Email">
+                                            </label>
+                                            <button type="submit" class="inline-flex items-center justify-center leading-none bg-red-600 border border-transparent rounded-lg font-bold text-base text-white hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600/80 focus-visible:ring-offset-2 disabled:bg-red-600/50 disabled:cursor-not-allowed transition ease-in-out duration-300 px-6 py-4">
+                                                Join free
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
 
             <div class="flex justify-between items-center">
                 <h2 class="my-20 text-4xl font-bold">Most Read</h2>
